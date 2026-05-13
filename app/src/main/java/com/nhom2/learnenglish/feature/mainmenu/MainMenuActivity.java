5a68a34ae49e72c35cef9562aa1865f4a73a567f
Ducanth2006 <nicklazada22@gmail.com>
2026-05-14 06:53:46 +0700

    feat(ui)/designTwoGameToLearn

--- app/src/main/java/com/nhom2/learnenglish/feature/mainmenu/MainMenuActivity.java
+++ app/src/main/java/com/nhom2/learnenglish/feature/mainmenu/MainMenuActivity.java
@@ -20,8 +20,8 @@ import com.nhom2.learnenglish.core.util.AppExecutors;
 import com.nhom2.learnenglish.core.util.Navigator;
 import com.nhom2.learnenglish.feature.articles.ArticlesActivity;
 import com.nhom2.learnenglish.feature.articles.ArticleDetailActivity;
-import com.nhom2.learnenglish.ui.activity.LibraryActivity;
-import com.nhom2.learnenglish.ui.activity.WordSetDetailActivity;
+import com.nhom2.learnenglish.feature.wordsets.LibraryActivity;
+import com.nhom2.learnenglish.feature.wordsets.WordSetDetailActivity;
 
 import java.util.List;
 
