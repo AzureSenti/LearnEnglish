a0a89cfafb61efc63fee3781f06f2bbbaa5ab590
Satsukki <pming1250@gmail.com>
2026-05-14 22:47:27 +0700

    #14 add lien ket

---
+++ app/src/main/java/com/nhom2/learnenglish/core/data/local/dao/WordSetCrossDao.kt
@@ -0,0 +1,9 @@
+package com.nhom2.learnenglish.core.data.local.dao
+
+import androidx.room3.Dao
+import com.nhom2.learnenglish.core.data.local.entity.WordSetCrossRef
+
+@Dao
+interface WordSetCrossDao : BaseDao<WordSetCrossRef> {
+
+}
