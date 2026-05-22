b0740eabba4199cc9f96130e4caab92f76f5be7a
Ducanth2006 <nicklazada22@gmail.com>
2026-05-22 10:57:46 +0700

    delete redundant code

--- app/src/main/java/com/nhom2/learnenglish/core/util/SessionManager.kt
+++ app/src/main/java/com/nhom2/learnenglish/core/util/SessionManager.kt
@@ -5,17 +5,8 @@ import android.content.SharedPreferences
 
 class SessionManager(context: Context) {
     private val prefs: SharedPreferences = context.getSharedPreferences("user_session", Context.MODE_PRIVATE)
-    // Thêm hàm này để sau này người làm form Login sẽ gọi nó
-    fun saveUserId(userId: Long) {
-        prefs.edit().putLong("user_id", userId).apply()
-    }
 
-    // Sửa hàm này thành như sau:
-    fun getUserId(): Long {
-        // Tạm thời trả về 1L (ID của Guest) nếu user chưa đăng nhập.
-        // Bằng cách này, Game của bạn vẫn lưu được dữ liệu xuống DB local ở chế độ "Log without login".
-        return prefs.getLong("user_id", 1L)
-    }
+
 
     fun createLoginSession(token: String, userId: Long) {
         prefs.edit()
